a64d16634056bb2126b461ef96388fe08ac5e746
minali0916 <58200589+minali0916@users.noreply.github.com>
2019-11-25 22:47:41 -0500

    Add files via upload

---
+++ Carbon Footprint Calculator.py
@@ -0,0 +1,24 @@
+import cgitb
+cgitb.enable()
+
+food=input("Insert a type of food:")
+times=input("How many times do you eat this food in a week?")
+
+if food=="chicken" and times=="1":
+    print("Your carbon emission= 106 kg")
+
+if food=="chicken" and times=="2":
+    print("Your carbon emission= 106 kg")
+
+if food=="chicken" and times=="3":
+    print("Your carbon emission= 248 kg")
+
+if food=="chicken" and times=="4":
+    print("Your carbon emission= 248 kg")
+
+if food=="chicken" and times=="5":
+    print("Your carbon emission= 248 kg")
+
+
+
+
